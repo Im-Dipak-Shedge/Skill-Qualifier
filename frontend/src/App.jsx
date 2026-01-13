9692c6bbc08d395a4954bf9f62fc0f15dbf8bcd2
Dipak Shedge <dipakshedge20@gmail.com>
2026-01-13 17:51:09 +0530

    created backend folder structure and google with sign in on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
 import Signup from "./pages/Signup";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <>
-      <Signup />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
